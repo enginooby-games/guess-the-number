cdc3855966c36521ce9e4a33c5e841539aa7546d
cpulover <justacpulover@gmail.com>
2020-06-29 21:16:15 +0200

    .

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void loadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);  
+        }
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scenes/NumberWizard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = Random.Range(min, max+1);
+        guessText.text = guess.ToString();
+    }
+
+    public void onPressHiger()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+}
